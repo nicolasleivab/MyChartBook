77e51a97f1664e5150a6bb83ba00327f9ac99dbd
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2020-05-02 01:42:16 -0400

    create and update profile route

---
+++ models/Profile.js
@@ -0,0 +1,88 @@
+const mongoose = require("mongoose");
+
+const experienceSchema = new mongoose.Schema({
+  title: {
+    type: String,
+  },
+  company: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  from: {
+    type: Date,
+  },
+  to: {
+    type: Date,
+  },
+  current: {
+    type: Boolean,
+    default: false,
+  },
+  description: {
+    type: String,
+  },
+});
+
+const educationSchema = new mongoose.Schema({
+  title: {
+    type: String,
+  },
+  institution: {
+    type: String,
+  },
+  year: {
+    type: Date,
+  },
+});
+
+const socialSchema = new mongoose.Schema({
+  twitter: {
+    type: String,
+  },
+  linkedin: {
+    type: String,
+  },
+  facebook: {
+    type: String,
+  },
+  instagram: {
+    type: String,
+  },
+  youtube: {
+    type: String,
+  },
+});
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  company: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: [String],
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  experience: [experienceSchema],
+  education: [educationSchema],
+  social: socialSchema,
+});
+
+module.exports = mongoose.model("profile", ProfileSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,98 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+const { check, validationResult } = require("express-validator");
 
-// @route  GET api/auth
-// @desc   Test route for profile
-// @access Public
-router.get("/", (req, res) => res.send("profile route"));
+// @route  GET api/profile/me
+// @desc   Get current user's profile
+// @access Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["username", "avatar"]);
+
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ msg: "There is currently no profile for this user" });
+    }
+    //send profile if exists
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route  POST api/profile
+// @desc   Create or update user profile
+// @access Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required").not().isEmpty(),
+      check("skills", "Skills is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      website,
+      location,
+      status,
+      skills,
+      bio,
+      experience,
+      education,
+      social,
+    } = req.body;
+
+    //build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (status) profileFields.status = status;
+    if (skills) profileFields.skills = skills;
+    //propertyFields.skills = skills.split(',)
+    if (bio) profileFields.bio = bio;
+    if (experience) profileFields.experience = experience;
+    if (education) profileFields.education = education;
+    if (social) profileFields.social = social;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      if (profile) {
+        //Update
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+
+        return res.json(profile);
+      }
+      //Create profile
+      profile = new Profile(profileFields);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
