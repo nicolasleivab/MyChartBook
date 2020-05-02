4532156809061a030ee86e773b69643cf60a26ae
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2020-05-02 01:58:39 -0400

    get all profiles, get profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -95,4 +95,42 @@ router.post(
   }
 );
 
+// @route  GET api/profile
+// @desc   Get all profiles
+// @access Public
+
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", [
+      "username",
+      "avatar",
+    ]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route  GET api/profile/user/:user_id
+// @desc   Get user's profile by id
+// @access Public
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id, //get user id from url params
+    }).populate("user", ["username", "avatar"]);
+    if (!profile) {
+      return res.status(400).json("Profile not found");
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(400).json("Profile not found"); // for not valid objectids
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
