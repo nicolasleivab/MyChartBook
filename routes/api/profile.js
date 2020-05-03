6d7ca4f003464b10209f60b17009b4b3f02a44b4
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2020-05-03 06:50:20 -0400

    delete experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -204,4 +204,27 @@ router.put(
   }
 );
 
+// @route  DELETE api/profile/experience/:exp_id
+// @desc   Delete experience from profile
+// @access Private
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.experience
+      .map((e) => e.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
