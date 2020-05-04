649f896fdb458182b1bc79097b452e49b85bd7b1
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2020-05-04 04:16:32 -0400

    implementing post and get posts routes

--- models/Post.js
+++ models/Post.js
@@ -9,7 +9,7 @@ const PostSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  name: {
+  username: {
     type: String,
   },
   avatar: {
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,55 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const Profile = require("../../models/Profile");
+const Post = require("../../models/Post");
+const { check, validationResult } = require("express-validator");
 
-// @route  GET api/auth
-// @desc   Test route for posts
-// @access Public
-router.get("/", (req, res) => res.send("posts route"));
+// @route  POST api/posts
+// @desc   Create a post
+// @access Private
+router.post(
+  "/",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text,
+        username: user.username,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// @route  GET api/posts
+// @desc   Get posts
+// @access Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
